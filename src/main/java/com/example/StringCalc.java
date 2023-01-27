fee8237446ce6829a0e0a0cc5257eb22a0d01e8f
Stefan Karlsson <phtefan.karlsson@gmail.com>
2023-01-27 15:30:20 +0100

    Finish code for test three so test two works after refactoring

--- src/main/java/com/example/StringCalc.java
+++ src/main/java/com/example/StringCalc.java
@@ -8,6 +8,9 @@ public class StringCalc {
 
         if (numbers.isEmpty())
             return 0;
+        if (numbers.length() == 1) {
+            return Integer.parseInt(numbers);
+        }
         else {
             return Integer.parseInt(stringNums[0]) + Integer.parseInt(stringNums[1]);
         }
